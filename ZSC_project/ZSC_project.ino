b4849d2f61e782e6c743c31fa6e971103fcb2ee1
Magdalena Pastuła <pastula.magdalena@gmail.com>
2021-05-25 13:43:04 +0200

    Added reading html file in Arduino from SD card.

--- ZSC_project/ZSC_project.ino
+++ ZSC_project/ZSC_project.ino
@@ -2,6 +2,8 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Ethernet.h>
+#include <SPI.h>
+#include <SD.h>
 
 #define FIXED_POINT_ACCURACY  1000
 #define SHTC3_MAX_VALUE       65535
@@ -19,8 +21,8 @@
 uint8_t SHTC3_MEAS_COMM[SHTC3_MSG_LEN]= {0x7C, 0xA2};
 
 char ZPH01_value[10];
-char ZPH01_values[ZPH01_VALUES_NO][10];
-uint8_t ZPH01_values_index = 0;
+//char ZPH01_values[ZPH01_VALUES_NO][10];
+//uint8_t ZPH01_values_index = 0;
 
 int ZP01_value;
 
@@ -31,7 +33,7 @@ uint16_t SHTC3_hum_value;
 SoftwareSerial ZPH01_serial(2, 3);  // RX, TX
 uint8_t ZPH01_buffer[ZPH01_MSG_LEN];
 uint8_t SHTC3_buffer[SHTC3_MSG_LEN];
-char buffer[100];
+char buffer[75];
 
 uint16_t counter = 0;
 
@@ -40,8 +42,9 @@ byte mac[] = {
 };
 
 IPAddress ip(192, 168, 31, 177);
-
 EthernetServer server(80);
+
+File myFile;
 
 uint8_t ZPH01DV_checksum_correct(uint8_t* gotten_msg)
 {
@@ -60,10 +63,10 @@ uint8_t ZPH01DV_checksum_correct(uint8_t* gotten_msg)
   }
 }
 
-uint32_t SHTC3_convertTemperature(uint16_t temp) {
-//  return (temp*175.0/65535)-45;
-  return temp*175*FIXED_POINT_ACCURACY/(SHTC3_MAX_VALUE) - 45*FIXED_POINT_ACCURACY;
-}
+//uint32_t SHTC3_convertTemperature(uint16_t temp) {
+////  return (temp*175.0/65535)-45;
+//  return temp*175*FIXED_POINT_ACCURACY/(SHTC3_MAX_VALUE) - 45*FIXED_POINT_ACCURACY;
+//}
 
 void readSensors(void)
 {
@@ -72,15 +75,15 @@ void readSensors(void)
     ZPH01_serial.readBytes(ZPH01_buffer, ZPH01_MSG_LEN);
     if (ZPH01DV_checksum_correct(ZPH01_buffer))
     {
-      sprintf(buffer, "Received messege from ZPH01: %u.%02u \r\n", ZPH01_buffer[3], ZPH01_buffer[4]);
+      sprintf(buffer, "Received ZPH01 msg: %u.%02u \r\n", ZPH01_buffer[3], ZPH01_buffer[4]);
       sprintf(ZPH01_value, "%u.%02u", ZPH01_buffer[3], ZPH01_buffer[4]);
-      sprintf(ZPH01_values[ZPH01_values_index], "%u.%02u", ZPH01_buffer[3], ZPH01_buffer[4]);
-      ZPH01_values_index = (ZPH01_values_index+1)%ZPH01_VALUES_NO;
+//      sprintf(ZPH01_values[ZPH01_values_index], "%u.%02u", ZPH01_buffer[3], ZPH01_buffer[4]);
+//      ZPH01_values_index = (ZPH01_values_index+1)%ZPH01_VALUES_NO;
       Serial.print(buffer);
     }
     else
     {
-      Serial.print("Error in receiving data from ZPH01\r\n");
+      Serial.print("Error receiving ZPH01 data\r\n");
     }
   }
 
@@ -93,31 +96,31 @@ void readSensors(void)
     Serial.print(buffer);
 
     // read from SHTC1
-    Wire.beginTransmission(SHTC3_ADDRESS);
-    Wire.write(SHTC3_MEAS_COMM, 2);
-    Wire.endTransmission();
-    delay(100);
-    Wire.requestFrom(SHTC3_ADDRESS, SHTC3_MSG_LEN);
-    if (Wire.available() >= SHTC3_MSG_LEN) {
-      for (int i=0;i<6;i++) {
-        SHTC3_buffer[i] = Wire.read();  
-      }
-      SHTC3_temp_raw = (SHTC3_buffer[0]<<8)+SHTC3_buffer[1];
-      SHTC3_temp_value = SHTC3_convertTemperature(SHTC3_temp_raw);
-      SHTC3_hum_value = (SHTC3_buffer[3]<<8)+SHTC3_buffer[4];
-      
-      sprintf(buffer, "Received temp from SHTC3 raw: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
-      Serial.print(buffer);
-      sprintf(buffer, "Received temp from SHTC3 conv: %u . %u \r\n", SHTC3_temp_value/FIXED_POINT_ACCURACY, SHTC3_temp_value%FIXED_POINT_ACCURACY);
-      Serial.print(buffer);
-      sprintf(buffer, "Received hum from SHTC3 raw: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
-      Serial.print(buffer);
-    } 
-    else {
-      SHTC3_buffer[0] = 0;
-//      sprintf(buffer, "Received messege from SHTC3: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
-      Serial.print("Error in receiving data from SHTC3.\r\n");
-    }
+//    Wire.beginTransmission(SHTC3_ADDRESS);
+//    Wire.write(SHTC3_MEAS_COMM, 2);
+//    Wire.endTransmission();
+//    delay(100);
+//    Wire.requestFrom(SHTC3_ADDRESS, SHTC3_MSG_LEN);
+//    if (Wire.available() >= SHTC3_MSG_LEN) {
+//      for (int i=0;i<6;i++) {
+//        SHTC3_buffer[i] = Wire.read();  
+//      }
+////      SHTC3_temp_raw = (SHTC3_buffer[0]<<8)+SHTC3_buffer[1];
+////      SHTC3_temp_value = SHTC3_convertTemperature(SHTC3_temp_raw);
+////      SHTC3_hum_value = (SHTC3_buffer[3]<<8)+SHTC3_buffer[4];
+////      
+////      sprintf(buffer, "Received temp SHTC3 raw: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
+////      Serial.print(buffer);
+////      sprintf(buffer, "Received temp SHTC3 conv: %u . %u \r\n", SHTC3_temp_value/FIXED_POINT_ACCURACY, SHTC3_temp_value%FIXED_POINT_ACCURACY);
+////      Serial.print(buffer);
+////      sprintf(buffer, "Received hum SHTC3 raw: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
+////      Serial.print(buffer);
+//    } 
+//    else {
+//      SHTC3_buffer[0] = 0;
+////      sprintf(buffer, "Received messege from SHTC3: %u \r\n", (SHTC3_buffer[0]<<8)+SHTC3_buffer[1]);
+//      Serial.print("Error receiving from SHTC3.\r\n");
+//    }
     
     counter = 0;
   }
@@ -141,17 +144,21 @@ void setup() {
   
   Serial.println("Hello!");
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.");
+    Serial.println("ETH shield not found.");
     while (true) {
-      delay(1); // do nothing
+      delay(1);
     }
   }
   if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
+    Serial.println("ETH cable not connected.");
   }
+  if (!SD.begin(4)) {
+    Serial.println("SD init failed.");
+    while (1);
+  }
+  
   server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+  Serial.println("Server ready");
 }
 
 void loop() {
@@ -176,28 +183,55 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.print("ZPH01 read value: ");
-          client.print(ZPH01_value);
-          client.println("<br />");
-          client.print("ZP01 read value: ");
-          client.print(ZP01_value);
-          client.println("<br />");
-          client.print("SHTC3 read temperature raw: ");
-          client.print(SHTC3_temp_raw);
-          client.println("<br />");
-          client.print("SHTC3 read temperature converted: ");
-          client.print(SHTC3_temp_value/FIXED_POINT_ACCURACY);
-          client.print(".");
-          client.print(SHTC3_temp_value%FIXED_POINT_ACCURACY);
-          client.println("<br/>");
-          client.print("SHTC3 read humidity: ");
-          client.print(SHTC3_hum_value);
-          client.println("<br />");
-          client.println("</html>");
+          client.println("Refresh: 30\n");  // refresh the page automatically every 10 sec
+          
+          myFile = SD.open("index1.txt");
+          if (myFile) {
+            while (myFile.available()) {
+              char read = myFile.read();
+//              Serial.write(read);
+              client.print((char)read);
+            }
+            // close the file:
+            myFile.close();
+            client.println("<br />");
+            client.print("ZP01 read value: ");
+            client.print(ZP01_value);
+            client.println("<br />");
+            client.println("  </body>\n</html>");
+//            myFile = SD.open("index2.txt");
+//            if (myFile) {
+//              while (myFile.available()) {
+//              char read = myFile.read();
+//              Serial.write(read);
+//              client.print((char)read);
+//            }
+//              // close the file:
+//              myFile.close();  
+//            }
+          } else {
+            // if the file didn't open, print an error:
+            Serial.println("Error opening index1.txt");
+          }
+//          client.println("<html>");
+//          client.print("ZPH01 read value: ");
+//          client.print(ZPH01_value);
+//          client.println("<br />");
+//          client.print("ZP01 read value: ");
+//          client.print(ZP01_value);
+//          client.println("<br />");
+//          client.print("SHTC3 read temperature raw: ");
+//          client.print(SHTC3_temp_raw);
+//          client.println("<br />");
+//          client.print("SHTC3 read temperature converted: ");
+//          client.print(SHTC3_temp_value/FIXED_POINT_ACCURACY);
+//          client.print(".");
+//          client.print(SHTC3_temp_value%FIXED_POINT_ACCURACY);
+//          client.println("<br/>");
+//          client.print("SHTC3 read humidity: ");
+//          client.print(SHTC3_hum_value);
+//          client.println("<br />");
+//          client.println("</html>");
           break;
         }
         if (c == '\n') {
@@ -218,25 +252,3 @@ void loop() {
     readSensors();
   }
 }
-
-//int main(void)
-//{
-//  Serial.begin(115200);
-////  while (!Serial) {
-////    ; // wait for serial port to connect. Needed for Native USB only
-////  }
-//  ZPH01_serial.begin(9600);
-//
-//  Serial.println("Hello!");
-//  while(true) 
-//  {
-//    ZPH01_serial.readBytes(ZPH01_buffer, MESSAGE_LEN);
-//    if (Serial.available())
-//    {
-//      Serial.write("Received message: ");
-//      Serial.write(ZPH01_buffer, MESSAGE_LEN); 
-//    }
-//  }
-//
-//  return 0;
-//}
